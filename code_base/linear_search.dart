2e94a0100cf6e73c417f9f97a3ae4fcb8a73550c
Alexandre Yano <alexandreyano@hotmail.com>
2021-07-21 19:08:32 -0400

    Update linear_search.dart

--- code_base/linear_search.dart
+++ code_base/linear_search.dart
@@ -8,14 +8,11 @@ Time Complexity:
 Best-case complexity: O(1) if the element is found at the first index.
 Worst-case complexity:  O(n) where the element if found at the last index.
 
-
 */
 void main(){
   
   linearSearch([12,8,3, 5, 100, 26, 20, 1, 3, 13, 50], 26);
 }
-
-
 
 int linearSearch(List<int> arr, int target ){
   for(int index = 0; index<arr.length; index++){
