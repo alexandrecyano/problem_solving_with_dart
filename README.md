79d5383898cb07c93100c022b54d7c62e1f20e4c
Alexandre Yano <alexandreyano@hotmail.com>
2021-08-15 13:58:02 -0400

    Array With Elements Not Equal to Average of Neighbors

--- README.md
+++ README.md
@@ -13,3 +13,6 @@
 [6 - Leetcode - Best Time to Buy and Sell Stock](/code_base/best_time_to_buy_and_sell_stock.dart)
 
 [7 - Leetcode  - Climbing Stairs](/code_base/climbing_stairs.dart)
+
+[8 - Leetcode  - Array With Elements Not Equal to Average of Neighbors](/code_base/array_with_elements_not_equal_av.dart)
+
---
+++ code_base/array_with_elements_not_equal_av.dart
@@ -0,0 +1,52 @@
+/*
+You are given a 0-indexed array nums of distinct integers. You want to rearrange the elements in the 
+array such that every element in the rearranged array is not equal to the average of its neighbors.
+More formally, the rearranged array should have the property such that for every i in the 
+range 1 <= i < nums.length - 1, (nums[i-1] + nums[i+1]) / 2 is not equal to nums[i].
+Return any rearrangement of nums that meets the requirements.
+
+
+Example 1:
+
+Input: nums = [1,2,3,4,5]
+Output: [1,2,4,5,3]
+Explanation:
+When i=1, nums[i] = 2, and the average of its neighbors is (1+4) / 2 = 2.5.
+When i=2, nums[i] = 4, and the average of its neighbors is (2+5) / 2 = 3.5.
+When i=3, nums[i] = 5, and the average of its neighbors is (4+3) / 2 = 3.5.
+Example 2:
+
+Input: nums = [6,2,0,9,7]
+Output: [9,7,6,2,0]
+Explanation:
+When i=1, nums[i] = 7, and the average of its neighbors is (9+6) / 2 = 7.5.
+When i=2, nums[i] = 6, and the average of its neighbors is (7+2) / 2 = 4.5.
+When i=3, nums[i] = 2, and the average of its neighbors is (6+0) / 2 = 3.
+
+*/
+
+//The approach is little different to the examples given, so output will be different from the examples as it changes [1, 2, 3, 4, 5] to [1, 5, 2, 4, 3].
+
+void main() {
+  rearrangeArray([3, 1, 2, 4, 5]);
+  rearrangeArray([6, 2, 0, 9, 7]);
+}
+
+List<int> rearrangeArray(List<int> arr) {
+  arr.sort();
+  List<int> result = [];
+  int l = 0;
+  int r = arr.length - 1;
+
+  while (arr.length != result.length) {
+    result.add(arr[l]);
+    l += 1;
+
+    if (l < r) {
+      result.add(arr[r]);
+      r -= 1;
+    }
+  }
+  print(result);
+  return result;
+}
